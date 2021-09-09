3db5137ae7ff9a0e788c267351d1bbcc0fdb924b
Julia Silge <julia.silge@gmail.com>
2021-09-09 14:45:08 -0600

    Update what generated Plumber file will look like

--- inst/plumber/biv_svm/plumber.R
+++ inst/plumber/biv_svm/plumber.R
@@ -2,13 +2,16 @@ library(deploytidymodels)
 library(pins)
 library(plumber)
 
-library(workflows)  ## eventually we will be able to extract all these from the model
-library(recipes)
-library(parsnip)
-library(LiblineaR)
+if (FALSE) {
+    library(workflows)  ## eventually we will be able to extract all these from the model
+    library(recipes)
+    library(parsnip)
+    library(LiblineaR)
+}
 
 model_board <- board_rsconnect()
 m <- model_board %>% modelops_pin_read("julia.silge/biv_svm")
+## stopifnot for specific plain text version/hash???
 
 #* @plumber
 function(pr) {
